c4b6ffe2025b247b37fbd110cd75c4eae8fe81e8
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-29 01:25:22 +0600

    display fitness component

--- src/components/Fitness/Fitness.js
+++ src/components/Fitness/Fitness.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Fitness = () => {
 	return (
-		<div>
-			<h2>fitness</h2>
+		<div className="fitness text-white d-flex text-center  align-items-center justify-content-around mt-5">
+			<div className="weight">
+				<p className="mb-1">
+					<span className="fitness-font">75</span>kg{' '}
+				</p>
+				<p>Weight</p>
+			</div>
+			<div className="height">
+				<p className="mb-1">
+					<span className="fitness-font">6.2</span>kg{' '}
+				</p>
+				<p>Height</p>
+			</div>
+			<div className="age">
+				<p className="mb-1">
+					<span className="fitness-font">22</span>yrs{' '}
+				</p>
+				<p>Age</p>
+			</div>
 		</div>
 	);
 };
