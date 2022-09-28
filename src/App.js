1ad456af1ddbe22f9736582f3b1e2638cfc5fbd4
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-28 22:46:29 +0600

    header component complete and install lato font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
 	return (
 		<div className="App">
-			<h2>
-				<i class="fa-solid fa-globe"></i>hello
-			</h2>
+			<Header></Header>
 		</div>
 	);
 }
