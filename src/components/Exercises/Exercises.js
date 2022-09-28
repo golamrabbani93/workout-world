fbf4c98c8dbc4d61d1c5e8d73ddead382d28036f
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-28 23:13:16 +0600

    send data Exercises to Exercise Components

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Exercise from '../Exercise/Exercise';
 
 const Exercises = () => {
 	const [exercizes, setExercizes] = useState([]);
@@ -12,8 +13,14 @@ const Exercises = () => {
 			<h2>Exercises</h2>
 			<div className="container-fluid">
 				<div className="row">
-					<div className="col-xl-8"></div>
-					<div className="col-xl-4">2</div>
+					<div className="col-xl-9">
+						<div className="row row-cols-1 row-cols-md-3 g-4">
+							{exercizes.map((exercise) => (
+								<Exercise exercise={exercise} key={exercise.id}></Exercise>
+							))}
+						</div>
+					</div>
+					<div className="col-xl-3">2</div>
 				</div>
 			</div>
 		</div>
