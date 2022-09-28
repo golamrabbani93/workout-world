1f264ca0dc7a054c6457c8120ec08b5da61e900e
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-28 23:52:51 +0600

    make data dynamic card and make card button

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -17,9 +17,9 @@ const Exercise = (props) => {
 					<h5 className="card-title fw-bolder">{name}</h5>
 					<p className="card-text fw-bold">Time required : {time}s</p>
 				</div>
-				{/* <div className="list-btn">
+				<div className="list-btn">
 					<button className="btn btn-primary w-75 d-block m-auto mb-3">Add to list</button>
-				</div> */}
+				</div>
 			</div>
 		</div>
 	);
