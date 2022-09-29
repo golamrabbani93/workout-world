f43683a7fcfeaa5a2263f559bc2c6618c80c4fff
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-29 14:47:16 +0600

    create questions component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Exercises from './components/Exercises/Exercises';
 import Header from './components/Header/Header';
+import Questions from './components/Questions/Questions';
 
 function App() {
 	return (
 		<div className="App">
 			<Header></Header>
 			<Exercises></Exercises>
+			<Questions></Questions>
 		</div>
 	);
 }
---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Questions = () => {
+	return (
+		<div className="container">
+			<h2>Questions</h2>
+		</div>
+	);
+};
+
+export default Questions;
